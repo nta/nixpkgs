44fb8dc9f477f7da81ced98487991d809b44fea7
K900 <me@0upti.me>
2026-01-13 20:42:10 +0300

    kdePackages: Plasma 6.5.5 -> 6.6 (beta)

--- pkgs/kde/plasma/plasma-workspace/default.nix
+++ pkgs/kde/plasma/plasma-workspace/default.nix
@@ -38,6 +38,11 @@ mkKdeDerivation {
     chmod -x $out/libexec/plasma-sourceenv.sh
   '';
 
+  extraCmakeFlags = [
+    "-DGLIBC_LOCALE_GEN=OFF"
+    "-DGLIBC_LOCALE_PREGENERATED=ON"
+  ];
+
   extraNativeBuildInputs = [
     pkg-config
     spirv-tools
